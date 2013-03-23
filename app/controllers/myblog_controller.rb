a18b6024ac7c6565ad89c82b9abf8ce50f3abbf1
YangHang <i.am.yanghang@gmail.com>
2013-03-23 18:24:57 +0800

    finish the theme

---
+++ app/controllers/myblog_controller.rb
@@ -0,0 +1,10 @@
+class MyblogController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def connect
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Blog::Application.routes.draw do
+  get "myblog/home"
+
+  get "myblog/about"
+
+  get "myblog/connect"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +54,7 @@ Blog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'myblog#home'
 
   # See how all your routes lay out with "rake routes"
 
